bb5906baeb0263133efc93e4bca8490bae833ea0
Alex Dykstra <alex@alexdykstra.com>
2022-06-23 09:52:32 -0400

    firebase commit

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0PuZwBSK9ni7quMHJal_S2LgLorQVY5E",
+  authDomain: "mancamp-ae1c1.firebaseapp.com",
+  projectId: "mancamp-ae1c1",
+  storageBucket: "mancamp-ae1c1.appspot.com",
+  messagingSenderId: "827010810787",
+  appId: "1:827010810787:web:d67813411784da86199016",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
